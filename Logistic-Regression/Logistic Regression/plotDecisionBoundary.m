c7334a76969504b709dea4dd5b6af8ade3705f59
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-18 18:12:42 -0500

    Update plotDecisionBoundary.m

--- Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
+++ Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
@@ -27,7 +27,7 @@ if size(X, 2) <= 3
     axis([30, 100, 30, 100])
 else
     % Here is the grid range
-    u = linspace(-1, 1.5, 50);
+    u = linspace(-1, 1.5, 50); 
     v = linspace(-1, 1.5, 50);
 
     z = zeros(length(u), length(v));
