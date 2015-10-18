eb3dd75c546a893d8f7a2e332dfb8c35fca7bc4a
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-18 18:12:50 -0500

    Update plotDecisionBoundary.m

--- Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
+++ Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
@@ -37,7 +37,7 @@ else
             z(i,j) = mapFeature(u(i), v(j))*theta;
         end
     end
-    z = z'; % important to transpose z before calling contour
+    z = z'; % important to transpose z before calling contour 
 
     % Plot z = 0
     % Notice you need to specify the range [0, 0]
