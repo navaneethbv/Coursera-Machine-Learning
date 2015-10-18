2bdc51787c1fd349995b065153d66c4ea0f5fe74
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-18 18:12:34 -0500

    Update plotDecisionBoundary.m

--- Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
+++ Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
@@ -17,7 +17,7 @@ if size(X, 2) <= 3
     plot_x = [min(X(:,2))-2,  max(X(:,2))+2];
 
     % Calculate the decision boundary line
-    plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1));
+    plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1)); 
 
     % Plot, and adjust axes for better viewing
     plot(plot_x, plot_y)
