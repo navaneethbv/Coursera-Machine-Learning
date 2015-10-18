984b959dc8876e1c7dd68a0e333678e2a1d475a3
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-18 18:12:27 -0500

    Update plotDecisionBoundary.m

--- Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
+++ Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
@@ -21,7 +21,7 @@ if size(X, 2) <= 3
 
     % Plot, and adjust axes for better viewing
     plot(plot_x, plot_y)
-    
+     
     % Legend, specific for the exercise
     legend('Admitted', 'Not admitted', 'Decision Boundary')
     axis([30, 100, 30, 100])
