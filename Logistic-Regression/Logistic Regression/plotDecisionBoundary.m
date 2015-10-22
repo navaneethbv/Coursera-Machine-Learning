439d0e948508749eea6eac85777cea8aa558fcf7
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-21 19:22:42 -0500

    Update plotDecisionBoundary.m

--- Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
+++ Logistic-Regression/Logistic Regression/plotDecisionBoundary.m
@@ -6,7 +6,7 @@ function plotDecisionBoundary(theta, X, y)
 %   a either 
 %   1) Mx3 matrix, where the first column is an all-ones column for the 
 %      intercept.
-%   2) MxN, N>3 matrix, where the first column is all-ones
+%   2) MxN, N>3 matrix, where the first column is all-ones 
   
 % Plot Data 
 plotData(X(:,2:3), y);
